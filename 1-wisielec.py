6f874179d8f6836ee7a96dfa3d08bd59a3102756
paulinakopec <100046393+paulinakopec@users.noreply.github.com>
2022-05-16 09:57:46 +0200

    Update 1-wisielec.py

--- 1-wisielec.py
+++ 1-wisielec.py
@@ -1,9 +1,9 @@
 import sys
 
-no_of_tries=5
-word="kamila"
-used_letters=[]
-user_word=[]
+no_of_tries = 5
+word = "kamila"
+used_letters = []
+user_word = []
 
 def find_indexes(word, letter):
     indexes = []
@@ -23,23 +23,23 @@ for _ in word:
     user_word.append("_")
 
 while True:
-    letter=input("Podaj litere: ")
+    letter = input("Podaj litere: ")
     used_letters.append(letter)
 
-    found_indexes=find_indexes(word, letter)
+    found_indexes = find_indexes(word, letter)
 
     if len(found_indexes) == 0:
         print(f"Nie ma takiej litery jak {letter}")
-        no_of_tries-=1
+        no_of_tries -= 1
          
 
-        if no_of_tries== 0:
+        if no_of_tries == 0:
             print("Koniec gry :(")
             sys.exit(0)
     
     else:
         for index in found_indexes:
-            user_word[index]=letter
+            user_word[index] = letter
     
     show_state_of_game()
        
